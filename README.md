86173c9137f4884eb33d1cdedb55936a583f2961
Jesse Snyder <jessetsnyder@gmail.com>
2020-06-16 22:32:47 -0600

    working first version

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 ## roadmap
 
+- [ ] write to github gist
+
 - [ ] restrict bot to 1 channel
 - [ ] run bot from remote server
 - [ ] debugger
 - [ ] tests
+- [ ] write functionally
--- index.js
+++ index.js
@@ -1,21 +1,57 @@
-require('dotenv').config()
-const Discord = require('discord.js')
+const dotenv = require('dotenv')
 const _ = require('lodash')
+const Discord = require('discord.js')
+const GistClient = require('gist-client')
 
-const TOKEN = process.env.BOT_TOKEN
+// env
+dotenv.config()
+const {
+  BOT_TOKEN,
+  GIST_FILENAME,
+  GIST_TOKEN,
+  GIST_ID,
+} = process.env
+
+// init clients
 const bot = new Discord.Client()
+const gistClient = new GistClient()
 
-bot.login(TOKEN)
+// login clients
+bot.login(BOT_TOKEN)
+gistClient.setToken(GIST_TOKEN)
 
-bot.on('messageUpdate', (old, current) => {
+// msg handler
+bot.on('messageUpdate', async (old, current) => {
   const path = 'embeds[0].url'
   const oldUrl = _.get(old, path)
   const currentUrl = _.get(current, path)
 
   if (!oldUrl && currentUrl && currentUrl.includes('themoviedb.org/movie')) {
-    console.log(currentUrl);
-    // TODO - post to github
     const title = _.get(current, 'embeds[0].title')
     current.reply(`say, you wanted to download ${title}. is that correct?`)
+
+    const gist = await gistClient.getOneById(GIST_ID)
+
+    const datum = {
+      title: title,
+      url: currentUrl,
+    }
+
+    const oldContent = gist.files[GIST_FILENAME].content
+
+    const movieList = JSON.parse(oldContent)
+
+    const updatedMovieList = movieList.concat([datum])
+
+    const request = {
+      files: {
+        [GIST_FILENAME]: {
+          content: JSON.stringify(updatedMovieList),
+        },
+      },
+    }
+
+    const newGist = await gistClient.update(GIST_ID, request)
+
   }
 })
